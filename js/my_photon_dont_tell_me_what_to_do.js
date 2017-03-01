5ed32f260be47a62e4d3922438eaef07f8bcdd98
phillycheese007 <philipdusel@gmail.com>
2017-03-01 12:49:09 -0500

    Update my_photon_dont_tell_me_what_to_do.js

--- js/my_photon_dont_tell_me_what_to_do.js
+++ js/my_photon_dont_tell_me_what_to_do.js
@@ -2,20 +2,13 @@ var $body,
 	$demos,
 	$crane,
 	$map,
-	$mapPanel1,
-	$mapPanel2,
-	$mapPanel3,
-	$mapCover,
-	$coverflow,
 	$toggleBtn,
 	$toggleOn,
 	$toggleOff,
 	crane,
 	craneFaces,
 	cubeFaces,
-	map,
 	diamondFaces,
-	coverflowFaces,
 	shadeAmount,
 	tintAmount,
 	light,
@@ -45,7 +38,6 @@ $(document).ready(function() {
 	light = new Photon.Light();
 	shadeAmount = .5;
 	tintAmount = 0;
-	coverflowFaces = [];
 	cubeFaces = [];
 	diamondFaces = [];
 	currentCover = 0;
@@ -61,9 +53,7 @@ $(document).ready(function() {
 	transitionEndEvent = transitionEndEvents[domTransitionProperty];
 
 	setupLightControls();
-	setupCoverflow();
 	setupCrane();
-	setupMap();
 
 	// demo menu
 	$('.example-menu a').bind('click', onDemoNav);
@@ -144,25 +134,11 @@ function onDemoNav(e) {
 	$(this).addClass('current');
 
 	switch(demo) {
-		case 'coverflow':
-			hideCrane();
-			hideMap();
-			showCoverflow();
-			renderCurrent = renderCoverflow;
-			break;
 		case 'crane':
-			hideCoverflow();
-			hideMap();
 			showCrane();
 			renderCurrent = renderCrane;
 			break;
-		case 'map':
-			hideCoverflow();
-			hideCrane();
-			showMap();
-			renderCurrent = renderMap;
-			break;
-	}
+
 
 	renderCurrent();
 	if(!isLit) {
@@ -213,159 +189,6 @@ function rotateCrane(e) {
 
 
 
-
-
-
-
-
-
-/*---------------------------------
-
-	Map
-
----------------------------------*/
-
-function setupMap() {
-	$map = $('.map');
-	$mapPanel1 = $('.panel-1');
-	$mapPanel2 = $('.panel-2');
-	$mapPanel3 = $('.panel-3');
-	$mapCover = $('.map-cover');
-	$map.bind('click', toggleMap);
-
-	map = new Photon.FaceGroup($('.map')[0], $('.map .face'), 1.5, .2, true);
-	renderMap();
-}
-
-function toggleMap() {
-	$map.toggleClass('is-open');
-
-	$map.unbind();
-	$map.bind(transitionEndEvent, stopRenderTimer);
-
-	if(!renderTimer) {
-		renderTimer = setInterval(renderMap, 34);
-	}
-}
-
-function renderMap() {
-	map.render(light, true, true);
-}
-
-function showMap() {
-	$body.bind('mousemove', rotateMap);
-	$map.show();
-}
-
-function hideMap() {
-	$body.unbind('mousemove', rotateMap);
-	$map.hide();
-}
-
-function rotateMap(e) {
-	var xPer = e.pageX / $body.width();
-	var yPer = e.pageY / $body.height();
-
-	$mapPanel1.css(cssTransformProperty, 'rotateY(' + (178 - (138 * xPer)) + 'deg)');
-	$mapCover.css(cssTransformProperty, 'rotateY(' + (178 - (138 * xPer)) + 'deg) translateZ(-2px) rotateY(180deg) translateX(240px)');
-	$mapPanel3.css(cssTransformProperty, 'rotateY(' + (178 - (138 * xPer)) + 'deg)');
-	$map.css(cssTransformProperty, 'rotateX(' + (40 - (yPer * 70)) + 'deg) rotateY(' + (20 - (xPer * 60)) + 'deg) rotateZ(0)');
-
-	renderMap();
-}
-
-
-
-
-
-
-
-
-
-/*---------------------------------
-
-	Coverflow
-
----------------------------------*/
-
-function setupCoverflow() {
-	$coverflow = $('.coverflow');
-	var $coverflowItems = $coverflow.find('li');
-
-	$coverflowItems.each(function(i) {
-		coverflowFaces[i] = new Photon.Face($(this)[0], shadeAmount);
-	});
-
-	console.log(transitionEndEvent);
-	$coverflowItems.eq(1).bind(transitionEndEvent, stopRenderTimer);
-
-	setCoverTransforms();
-}
-
-function changeCover() {
-	currentCover = currentCover < coverflowFaces.length - 1 ? currentCover + 1 : 0;
-	setCoverTransforms(true);
-}
-
-function setCoverTransforms(animate) {
-	if(!renderTimer && animate) {
-		renderTimer = setInterval(renderCoverflow, 34);
-	}
-	for(var i = 0; i < coverflowFaces.length; i++) {
-		var element = coverflowFaces[i].element;
-		var offset = Math.abs(currentCover - i);
-		var x = i == currentCover ? 0 : (150 + (100 * offset)) * (i < currentCover ? -1 : 1);
-		var z = i == currentCover ? 0 : -200;
-
-		var rotationY = i == currentCover ? 0 : (80 + (offset * -5)) * (i < currentCover ? 1 : -1);
-
-		$(element).css(cssTransformProperty, 'translateX(' + x +'px) translateZ(' + z + 'px) rotateY(' + rotationY + 'deg)');
-	}
-}
-
-function rotateCoverflow(e) {
-	var xPer = e.pageX / $body.width();
-
-	var newIndex = (coverflowFaces.length -1) - Math.round((coverflowFaces.length -1) * xPer);
-
-	if(!renderTimer && newIndex != currentCover) {
-		renderTimer = setInterval(renderCoverflow, 34);
-		currentCover = newIndex;
-	}
-	for(var i = 0; i < coverflowFaces.length; i++) {
-		var element = coverflowFaces[i].element;
-		var offset = Math.abs(currentCover - i);
-		var x = i == currentCover ? 0 : (150 + (100 * offset)) * (i < currentCover ? -1 : 1);
-		var z = i == currentCover ? 0 : -200;
-
-		var rotationY = i == currentCover ? 0 : (80 + (offset * -5)) * (i < currentCover ? 1 : -1);
-
-		$(element).css(cssTransformProperty, 'translateX(' + x +'px) translateZ(' + z + 'px) rotateY(' + rotationY + 'deg)');
-	}
-}
-
-function stopRenderTimer() {
-	if(renderTimer) {
-		clearInterval(renderTimer);
-		renderTimer = null;
-	}
-}
-
-function renderCoverflow() {
-	for(var i = 0; i < coverflowFaces.length; i++) {
-		coverflowFaces[i].render(light, true);
-	}
-}
-
-function hideCoverflow() {
-	$coverflow.hide();
-	$body.unbind();
-}
-
-function showCoverflow() {
-	$coverflow.show();
-	$body.bind('mousemove', rotateCoverflow);
-}
 
 
 
